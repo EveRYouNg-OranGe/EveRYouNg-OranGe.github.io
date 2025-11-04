7ebbf4ef5f27627a9c7aed5708828790616faa35
EveRYouNg-OranGe <2691781800@qq.com>
2025-11-04 14:56:03 +0800

    Site updated: 2025-11-04 14:55:59

---
+++ js/music.js
@@ -0,0 +1,9 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    audio: [{
+        name: '圈',
+        artist: '嘎调',
+        url: 'http://music.163.com/song/media/outer/url?id=355946.mp3',
+        cover: 'https://p1.music.126.net/ESPHttjXVsG3ZIU529lN-A==/109951163598339222.jpg?param=130y130',
+    }]
+});
